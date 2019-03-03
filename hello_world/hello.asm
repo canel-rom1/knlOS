a3ab5daa4cf8a4494c6eb892a0e03d6f2d635403
Rom1 <rom1@canel.ch>
2019-03-03 21:18:22 +0100

    Créeation d'une fonction dans hello_world

--- hello_world/hello.asm
+++ hello_world/hello.asm
@@ -2,15 +2,23 @@ section .text
 global	_start
 
 _start:
-	mov	ecx, message
-	mov	edx, length
-	mov	ebx, 1
-	mov	eax, 4
-	int	0x80
+	mov	eax, message
+	mov	ebx, length
+	call	print_str
 
 	mov	ebx, 0
 	mov	eax, 1
 	int	0x80
+
+print_str:
+	pusha
+	mov	ecx,eax
+	mov	edx,ebx
+	mov	eax,4
+	mov	ebx,1
+	int	0x80
+	popa
+	ret
 
 section	.data
 	message db 'Hello CANEL',10
