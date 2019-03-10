6c786a1b68b1833be032175592c317f607cc1ac4
Rom1 <rom1@canel.ch>
2019-03-10 20:29:34 +0100

    Initialisation des segments

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,9 +1,20 @@
 bits 16
-org 0x7C00
+org 0x0
 
+; Initialiser les segments
+	mov	ax, 0x07C0 ; Adresse de départ
+	mov	ds, ax ; Data segment
+	mov	es, ax ; Extra segment
+	;Pile
+	mov	ax, 0x9000
+	mov	ss, ax
+	mov	sp, 0xf000
+
+; Initialiser le disque
 	mov ah, 0
 	int 0x13
 
+; Charger le noyau
 start:
 	mov bx, 0x8000
 	mov al, 0x01
@@ -13,6 +24,7 @@ start:
 	mov ah, 0x02
 	int 0x13
 
+; Démarre le noyau
 	jmp 0x8000
 
 	times 510 - ($-$$) db 0
