f6d06f4f39693a12d870d0982108680401e5977f
Rom1 <rom1@canel.ch>
2019-03-10 20:48:55 +0100

    Créer libgraph pour faire des fonctions graphiques

--- bootloader/kernel.asm
+++ bootloader/kernel.asm
@@ -2,61 +2,7 @@ bits 16
 org 0x8000
 
 	jmp	start
-
-display_enable:
-	push	bp
-	mov	bp,sp
-
-	mov	ah,0x0
-	mov	al,0x7
-	int	0x10
-
-	mov	sp,bp
-	pop	bp
-	ret
-
-print:
-	push	bp
-	mov	bp,sp
-
-	mov	si,[bp+4]
-
-.loop:
-	lodsb
-	cmp	al,0
-	je	.end
-
-	mov	ah,0x0e
-	mov	bx,0
-	int	0x10
-
-	jmp	.loop
-
-.end:
-	mov	sp,bp
-	pop	bp
-	ret
-
-println:
-	push	bp
-	mov	bp,sp
-
-	push	word [bp+4]
-	call	print
-	add	sp,2
-
-	mov	ah,0x03
-	int	0x10
-
-	inc	dh
-	mov	dl,0
-	
-	mov	ah,0x2
-	int	0x10
-
-	mov	sp,bp
-	pop	bp
-	ret
+	%include "libgraph.asm"
 
 start:
 	
